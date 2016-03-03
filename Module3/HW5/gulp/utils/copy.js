dc08fa4f7dd58334024ad22096f37fc4276d0243
Stanislav Dolgachov <Stanislav_Dolgachov@epam.com>
2016-03-03 17:38:06 +0200

    M3/HW5: fix: removed unused code from utils for gulp tasks

--- Module3/HW5/gulp/utils/copy.js
+++ Module3/HW5/gulp/utils/copy.js
@@ -1,10 +1,8 @@
 'use strict';
 
 var gulp = require('gulp');
-var reload = require('browser-sync').reload;
 
 module.exports = function copyFiles(from, to) {
     gulp.src(from)
-        .pipe(gulp.dest(to))
-        .pipe(reload({stream: true}));
+        .pipe(gulp.dest(to));
 };
