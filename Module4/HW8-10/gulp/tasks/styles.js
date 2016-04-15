def41141109abc4fb22e51e9201641186fe2434c
Stanislav Dolgachov <Stanislav_Dolgachov@epam.com>
2016-04-15 17:19:33 +0300

    M4/HW8-10: refactoring

--- Module4/HW8-10/gulp/tasks/styles.js
+++ Module4/HW8-10/gulp/tasks/styles.js
@@ -22,22 +22,13 @@ const styleLintConfig = {
     "rules": {
         "block-no-empty": true,
         "color-no-invalid-hex": true,
-        "declaration-colon-space-after": "always",
         "declaration-colon-space-before": "never",
-        "function-comma-space-after": "always",
-        "function-url-quotes": "double",
         "media-feature-colon-space-after": "always",
         "media-feature-colon-space-before": "never",
-        "media-feature-name-no-vendor-prefix": true,
+        "media-feature-name-no-vendor-prefix": true ,
         "max-empty-lines": 5,
         "number-leading-zero": "never",
-        "number-no-trailing-zeros": true,
-        "property-no-vendor-prefix": true,
-        "selector-list-comma-space-before": "never",
-        "selector-list-comma-newline-after": "always",
-        "selector-no-id": true,
-        "string-quotes": "double",
-        "value-no-vendor-prefix": true
+        "selector-no-id": true
     }
 };
 
@@ -47,13 +38,13 @@ gulp.task('styles', function () {
 
 gulp.task('build:styles', () => {
     const processors = [
+        cssnano(),
+        stylelint(styleLintConfig),
+        reporter(),
         autoprefixer({
             browsers: ['last 3 versions', 'IE 8'],
             cascade: false
-        }),
-        cssnano()/*,
-        stylelint(styleLintConfig),
-        reporter()*/
+        })
     ];
 
     return gulp.src(src.styles)
